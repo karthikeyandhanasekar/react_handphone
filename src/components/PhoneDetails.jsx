8457e179cc8b6861ddbbf5c6f94e8f14ced57491
Karthik <dkkarthik2000@gmail.com>
2022-01-04 10:29:19 +0530

    details page

--- src/components/PhoneDetails.jsx
+++ src/components/PhoneDetails.jsx
@@ -1,12 +1,94 @@
-import { Button, Carousel, Typography} from "antd";
+import { Button, Carousel, Modal } from "antd";
+import Text from "antd/lib/typography/Text";
 import React from "react"
 import { useParams } from "react-router-dom"
 import { phones } from '../data/phones'
 import Header from './Header';
+import { database } from '../Firebase/firebaseconfig'
+import { setDoc, doc, deleteDoc, getDocFromServer } from "firebase/firestore";
+import { Link, useNavigate } from "react-router-dom";
 
 const PhoneDetails = () => {
     const { name } = useParams()
     const data = phones.find(ele => ele.title === name)
+    const navigate = useNavigate()
+    const [iscartsucess, setcartsucess] = React.useState(false)
+
+
+    const token = !!sessionStorage['auth-token']
+    //deletecart in firstore
+    const removecart = async (data) => {
+        try {
+            await deleteDoc(doc(database, "cart", sessionStorage.getItem("email"), "items", data.title))
+            setcartsucess(false)
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    //upload cart in firestore
+    const addcart = async (data) => {
+
+        try {
+            //if user signin
+            if (token) {
+                const document = doc(database, "cart", sessionStorage.getItem("email"), "items", data.title)
+                const docfromserver = await getDocFromServer(document)
+                if (docfromserver.exists()) {
+
+                    // const updatecart = async () => {
+                    //     let existdata = docfromserver.data()
+                    //     existdata["quantity"] += 1
+                    //     await updateDoc(document, existdata)
+                    // }
+
+                    //update existing cart
+
+                    Modal.info({
+                        title: "Already Added in cart",
+                        content: "Press Exit to enter shopping page or click ok to enter your cart",
+                        onOk() {
+                            //  updatecart()
+                            navigate('/cart')
+
+                        },
+                        onCancel() {
+                            navigate('/')
+                        }
+                    })
+                }
+                else {
+                    //add fresh cart
+                    const details = {
+                        id: data.id,
+                        name: data.title,
+                        updatedprice: data.price.toFixed(0),
+                        initialprice: data.price.toFixed(0),
+                        features: data.cpu + ", " + data.display + ", " + data.memory + ", " + data.battery,
+                        quantity: 1
+                    }
+                    await setDoc(document, details)
+                    setcartsucess(true)
+                }
+
+            }
+            else {
+                //if user not signin
+                Modal.info({
+                    title: "Login to continue your Shopping",
+                    onOk() {
+                        navigate('/login')
+                    },
+                    onCancel() {
+                        navigate('/')
+                    }
+                })
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
         <React.Fragment>
@@ -14,15 +96,17 @@ const PhoneDetails = () => {
             <main className="phonedetails">
                 <Carousel className="phonedisplay" autoplay dotPosition="bottom">
                     {
-                        data.images.map(ele => <img alt={ele} className="image" width={200} src={ele} />)
+                        data.images.map(ele => <img alt={ele} key={ele} className="image" width={200} src={ele} />)
                     }
                 </Carousel>
                 <div className="details">
                     <h1>{data.title}</h1>
-                    <Typography className="phonetext">{data.description}</Typography>
-                    <Typography className="phonetext"  type="success">Price : ₹ {data.price.toFixed(0)}</Typography>
-                    <Button type="primary">Add to Cart</Button>
-                    <Typography className="phonetext"  type="success" leve>Features</Typography>
+                    <Text className="phonetext">{data.description}</Text><br />
+                    <Text className="phonetext title" type="success">Price : ₹ {data.price.toFixed(0)}</Text>
+                    <br />
+                    <Button type="primary" danger={iscartsucess} onClick={() => { iscartsucess ? removecart(data) : addcart(data) }} > {iscartsucess ? `Remove from Cart` : `Add to Cart`}</Button>
+                    <br/>
+                    <Text className="phonetext title" type="success" >Features</Text>
                     <ul>
                         <li>{data.cpu}</li>
                         <li>{data.memory}</li>
